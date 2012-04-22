6c7caf00ee0d82e2c6e5fb21cc0f6177d56fab01
Andreas Jansson <andreas.s.t.jansson@gmail.com>
2012-04-22 20:37:49 +0100

    added midi to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name = 'Celltone',
-    version = '0.1',
+    version = '0.2',
     packages = ['celltone', 'tests'],
     entry_points = {
         'console_scripts': [
@@ -15,7 +15,11 @@ setup(
             ]
         },
     #install_requires = ['ply>=3.0', 'pyrex', 'pyPortMidi>=0.0.3', 'argparse>=1.0'],
-    install_requires = ['ply>=3.0', 'argparse>=1.0'],
+    install_requires = ['midi>=0.2.2', 'ply>=3.0', 'argparse>=1.0'],
+
+    dependency_links = [
+        'https://github.com/vishnubob/python-midi/tarball/master',
+        ],
 
     author = 'Andreas Jansson',
     author_email = 'andreas@jansson.me.uk',
